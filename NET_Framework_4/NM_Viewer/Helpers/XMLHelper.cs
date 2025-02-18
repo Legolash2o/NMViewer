99942893392841be587ad39a71a8d06f9cbf4e70
Liam Crozier <liamcrozier2005@hotmail.co.uk>
2025-02-18 21:36:28 +0000

    *folder changes

---
+++ NET_Framework_4/NM_Viewer/Helpers/XMLHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace NM_Viewer.Helpers
+{
+    public static class XMLHelper
+    {
+        public static T GetValue<T>(this XElement element, string attributeName)
+        {
+            try
+            {
+                XName name = XName.Get(attributeName);
+                var value = element.Attribute(name);
+                if (value == null)
+                    return default(T);
+
+                return (T)Convert.ChangeType(value.Value, typeof(T));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw e;
+            }
+
+            return default(T);
+        }
+
+        public static XElement GetElement(this XElement element, string elementName)
+        {
+            XName name = XName.Get(elementName);
+            return element.Element(name);
+        }
+
+
+    }
+}
---
+++ NET_Framework_4/NM_Viewer/Objects/Node.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+
+namespace NM_Viewer.Objects
+{
+    public class Node
+    {
+        #region CONSTRUCTOR
+        public Node()
+        {
+            Edges = new List<Edge>();
+            Stations = new List<Station>();
+        }
+        #endregion
+
+        #region PUBLIC PROPERTIES
+        public long Id { get; set; }
+
+        public double X { get; set; }
+
+        public double Y { get; set; }
+
+        public string Name { get; set; }
+
+        public bool Intersection
+        {
+            get
+            {
+                if (Edges.Count == 0)
+                    return false;
+
+                if (Edges.Count > 1)
+                    return true;
+
+                return false;
+            }
+        }
+
+
+        public List<Edge> Edges { get; set; }
+
+
+        public List<Station> Stations { get; set; }
+  
+        #endregion
+    }
+}
